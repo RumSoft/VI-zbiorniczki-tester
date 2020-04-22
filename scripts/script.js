e9bf3033da99432d6c22da708fe0c8c260e29227
Dominik Ruman <d@ruman.dev>
2020-04-22 18:53:21 +0200

    some improvements

--- scripts/script.js
+++ scripts/script.js
@@ -3,12 +3,12 @@ import logger from "./logger.js";
 import codeBox from "./codeMirror.js";
 
 //#region variables
-var inp = new Array(7)
+var inp = new Array(8)
   .fill("input")
   .map((x, i) => `${x}${i + 1}`)
   .map((x) => document.getElementById(x));
 
-var outp = new Array(7)
+var outp = new Array(8)
   .fill("output")
   .map((x, i) => `${x}${i + 1}`)
   .map((x) => document.getElementById(x));
@@ -21,10 +21,15 @@ var stopBtn = document.getElementById("stop");
 var variables = document.getElementById("variables");
 var deltatime = document.getElementById("deltatime");
 var stantextbox = document.getElementById("stantextbox");
+var stantextbox1 = document.getElementById("stantextbox1");
+var stantextbox2 = document.getElementById("stantextbox2");
+var stantextbox3 = document.getElementById("stantextbox3");
 var timtextbox = document.getElementById("timtextbox");
 var timtextbox1 = document.getElementById("timtextbox1");
 var timtextbox2 = document.getElementById("timtextbox2");
 var timtextbox3 = document.getElementById("timtextbox3");
+
+var oczkodiv = document.getElementById("oczko");
 
 var formatBtn = document
   .getElementById("formatBtn")
@@ -68,21 +73,52 @@ const io = {
 
 //init
 var forceStop = true;
-var stan = 1;
 var loopDelay = 100;
 var isRunning = false;
-var tim = 0;
-var tim1 = 0;
-var tim2 = 0;
-var tim3 = 0;
+
+function oczko(v) {
+  oczkodiv.style.opacity = Math.abs(v);
+  oczkodiv.style.filter = "";
+
+  if (v > 1) {
+    let vv = v * v;
+
+    let effects = {
+      "hue-rotate": `${(vv / 100).toFixed(2)}rad`,
+      invert: Math.sin(-vv) > 0 ? Math.sin(-vv) : 0,
+      saturate: Math.sin(vv / 5 + 1) * 2 + 2,
+      brightness: Math.sin(-vv / 10 + 2) / 3 + 1,
+      contrast: -Math.cos(vv / 5 + 11) * 5 + 6,
+    };
+
+    let eff = Object.keys(effects)
+      .map(
+        (x) =>
+          `${x}(${
+            typeof effects[x] === "number" ? effects[x].toFixed(2) : effects[x]
+          })`
+      )
+      .join(" ");
+    oczkodiv.style.filter = eff;
+  }
+
+  if (v < 0) oczkodiv.style.filter = `invert(${-v})`;
+}
 
 function loop() {
   if (forceStop) return;
-  stantextbox.innerText = stan;
-  timtextbox.innerText = ((tim * loopDelay) / 1000).toFixed(1);
-  timtextbox1.innerText = ((tim1 * loopDelay) / 1000).toFixed(1);
-  timtextbox2.innerText = ((tim2 * loopDelay) / 1000).toFixed(1);
-  timtextbox3.innerText = ((tim3 * loopDelay) / 1000).toFixed(1);
+  if (typeof stan !== "undefined") stantextbox.innerText = stan;
+  if (typeof stan1 !== "undefined") stantextbox1.innerText = stan1;
+  if (typeof stan2 !== "undefined") stantextbox2.innerText = stan2;
+  if (typeof stan3 !== "undefined") stantextbox3.innerText = stan3;
+  if (typeof tim !== "undefined")
+    timtextbox.innerText = ((tim * loopDelay) / 1000).toFixed(1);
+  if (typeof tim1 !== "undefined")
+    timtextbox1.innerText = ((tim1 * loopDelay) / 1000).toFixed(1);
+  if (typeof tim2 !== "undefined")
+    timtextbox2.innerText = ((tim2 * loopDelay) / 1000).toFixed(1);
+  if (typeof tim3 !== "undefined")
+    timtextbox3.innerText = ((tim3 * loopDelay) / 1000).toFixed(1);
 
   setTimeout(() => {
     var aK1 = input[0];
@@ -92,6 +128,8 @@ function loop() {
     var aK5 = input[4];
     var aK6 = input[5];
     var aK7 = input[6];
+    var aK8 = input[7];
+    var aK9 = input[8];
     var L1 = output[0];
     var L2 = output[1];
     var L3 = output[2];
@@ -99,6 +137,8 @@ function loop() {
     var L5 = output[4];
     var L6 = output[5];
     var L7 = output[6];
+    var L8 = output[7];
+    var L9 = output[8];
 
     try {
       eval(codeBox.doc.getValue());
@@ -115,6 +155,8 @@ function loop() {
     io.setOutput(4, L5);
     io.setOutput(5, L6);
     io.setOutput(6, L7);
+    io.setOutput(7, L8);
+    io.setOutput(8, L9);
 
     loop();
   }, loopDelay);
@@ -136,11 +178,11 @@ function stopProgram() {
   isRunning = false;
   codeBox.display.wrapper.style.pointerEvents = "unset";
 
-  stan = 1;
   io.reset();
 }
 
 function startProgram() {
+  oczko(0);
   startBtn.disabled = true;
   stopBtn.disabled = false;
   startBtn.blur();
@@ -154,7 +196,6 @@ function startProgram() {
   loopDelay = Math.max(1, Math.min(deltatime.value || 100, 1000));
   logger.Info(`Uruchamianie programu z cyklem ${loopDelay}ms`);
 
-  stan = 1;
   io.reset();
 
   if (variables.value)
@@ -181,7 +222,7 @@ function onKeyPress(ev) {
   if (document.activeElement === variables || document.activeElement === code)
     return;
 
-  if (ev.key >= "1" && ev.key <= "7") {
+  if (ev.key >= "1" && ev.key <= "8") {
     io.toggleInput(ev.key - 1);
   }
 }
